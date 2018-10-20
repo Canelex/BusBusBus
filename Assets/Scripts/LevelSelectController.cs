b8a03f6ece36d9d9b0a26128d4f4a1bdcc41f05a
Tim Kozlov <timkozlov@gmail.com>
2018-10-20 14:30:57 -0700

    Set Level Select Scene to 60fps

---
+++ Assets/Scripts/LevelSelectController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectController : MonoBehaviour
+{
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+}
